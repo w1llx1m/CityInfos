e11d074364c4e904c0b1d42153875273f464dfdf
w1llx1m <williamsilva.monteiro28@gmail.com>
2023-02-10 00:31:20 -0300

    adicionando o banco de dados

---
+++ CityInfo.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using CityInfo.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+        public DbSet<User> Users { get; set; }
+    }
+}
